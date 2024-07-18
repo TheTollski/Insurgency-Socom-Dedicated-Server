5e931fc2e62565e4200d4ca6039a7b67fe092217
Tollski <TheTollski@gmail.com>
2024-07-17 23:33:53 -0600

    Reduce fall damage, play demolition sound cues, show end round voting map images

--- Startup.bat
+++ Startup.bat
@@ -1,1 +1,1 @@
-start srcds.exe -usercon +maxplayers 16 +sv_lan 0 +map "crossroads_v5 firefight" -workshop +sv_pure 0
+start srcds.exe -usercon +maxplayers 16 +sv_lan 0 +map "crossroads_demolition_v2 skirmish" -workshop +sv_pure 0
